fa45da98969436ea37fe3ab7f688ffdea63a2d3e
saaguero <aguerosantiale@gmail.com>
2016-02-14 14:05:33 -0300

    Improve install script for backing if target files exists

--- install.sh
+++ install.sh
@@ -7,6 +7,9 @@ echo "creating symlinks"
 linkables=$( find -H "$DOTFILES" -maxdepth 3 -name '*.link' )
 for file in $linkables ; do
     target="$HOME/.$( basename $file ".link" )"
+    if [ -f "$target" ]; then
+      mv "${target}" "${target}.bak"
+    fi
     echo "creating symlink for $file"
-    ln -s "$file" "$target"
+    ln -s "${file}" "${target}"
 done
