9dabc990ae19a4a002a151155477c222536b1520
saaguero <aguerosantiale@gmail.com>
2016-04-29 22:17:26 -0300

    Add editorconfig defaults, improve install script

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 # Adapted from https://github.com/nicknisi/dotfiles
 
-DOTFILES=$HOME/dotfiles
-
 echo "creating symlinks"
-linkables=$( find -H "$DOTFILES" -maxdepth 3 -name '*.link' )
+linkables=$( find -H "." -maxdepth 3 -name '*.link' )
 for file in $linkables ; do
     target="$HOME/.$( basename $file ".link" )"
     if [ -f "$target" ]; then
